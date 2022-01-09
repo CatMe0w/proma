3da91768468c709c6f591ab03aaafbc807994abd
CatMe0w <CatMe0w@live.cn>
2022-01-09 23:27:26 +0800

    Make util.content_parser

--- util/content_parser.py
+++ util/content_parser.py
@@ -1,38 +1,29 @@
-def parse_text(item):
-    parsed_item = {}
-    return parsed_item
-
-
 def parse_url(item):
-    parsed_item = {}
-    return parsed_item
+    return {'url': item['link'], 'text': item['text']}
 
 
 def parse_emotion(item):
-    parsed_item = {}
-    return parsed_item
+    return {'id': item['text'], 'description': item['c']}
 
 
 def parse_image(item):
-    parsed_item = {}
-    return parsed_item
+    if item['type'] == '3':
+        return item['origin_src']
+    if item['type'] == '11':
+        return item['static']
+    if item['type'] == '20':
+        return item['src']
 
 
 def parse_video(item):
-    parsed_item = {}
-    return parsed_item
-
-
-def parse_audio(item):
-    parsed_item = {}
-    return parsed_item
+    return {'url': item['link'], 'cover_picture_url': item['src']}
 
 
 def parse(data):
     parsed_data = []
     for item in data:
         if item['type'] == '0' or '4' or '9':
-            parsed_data.append({'type': 'text', 'content': parse_text(item)})
+            parsed_data.append({'type': 'text', 'content': item['text']})
         if item['type'] == '1':
             parsed_data.append({'type': 'url', 'content': parse_url(item)})
         if item['type'] == '2':
@@ -42,5 +33,5 @@ def parse(data):
         if item['type'] == '5':
             parsed_data.append({'type': 'video', 'content': parse_video(item)})
         if item['type'] == '10':
-            parsed_data.append({'type': 'audio', 'content': parse_audio(item)})
+            parsed_data.append({'type': 'audio', 'content': item['voice_md5']})
     return parsed_data
