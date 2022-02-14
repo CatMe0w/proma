4748e51896a0a27b38dee3b58c1ff8c2b8b9f410
CatMe0w <CatMe0w@live.cn>
2022-02-15 03:30:56 +0800

    Add comments for clash_control

--- util/clash_control.py
+++ util/clash_control.py
@@ -2,13 +2,13 @@ import json
 import random
 import requests
 
-USE_CLASH = True
+USE_CLASH = True  # Clash/代理池总开关
 PROXIES = {
     "http": 'http://127.0.0.1:7890',
     "https": 'http://127.0.0.1:7890'
-}
-CLASH_API_URL = 'http://127.0.0.1:9090'
-PROXY_GROUP_NAME = 'Proxies'
+}  # HTTP代理地址
+CLASH_API_URL = 'http://127.0.0.1:9090'  # Clash RESTful API地址
+PROXY_GROUP_NAME = 'Proxies'  # Clash代理组名称；此外，请将此代理组的第一个节点设置为DIRECT，程序会首先使用直连，被限制后再使用代理
 
 if USE_CLASH:
     response = requests.get(CLASH_API_URL + '/proxies')
